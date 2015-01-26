199f216564f749347435c1375910e13d57fc11a9
pfirth <peter.firth@asu.edu>
2015-01-26 16:12:03 -0700

    Interface/Main Updates

--- Interface.py
+++ Interface.py
@@ -58,25 +58,76 @@ class Title(Widget):
         self.B = BoxLayout()
         self.L = Label(text = 'Title_Box', color = [1,0,0,1])
 
+    def Set_Text(self,txt):
+        self.L.text = txt
+
     def build(self):
         self.B.add_widget(self.L)
         return self.B
 
-class MFC_Container(object):
+class Process_Button(Widget):
+    def __init__(self):
+        self.B = BoxLayout()
+        self.Button = ToggleButton(text = 'Process_Button', color = [1,0,0,1])
+
+    def build(self):
+        self.B.add_widget(self.Button)
+        return self.B
+
+
+class Process_Container():
+    def __init__(self):
+        self.Container = BoxLayout(pos_hint = {'x':0.335},
+                         size_hint_x = 0.66, size_hint_y =  0.33)
+        self.Grid = GridLayout(rows = 1, cols = 4)
+
+        self.Vacuum_Button_Object = Process_Button()
+        self.Vacuum_Button = self.Vacuum_Button_Object.build()
+        self.Gas_Button_Object = Process_Button()
+        self.Gas_Button = self.Gas_Button_Object.build()
+        self.RF_Button_Object = Process_Button()
+        self.RF_Button = self.RF_Button_Object.build()
+        self.Move_Button_Object = Process_Button()
+        self.Move_Button = self.Move_Button_Object.build()
+
+
+    def build(self):
+        self.Grid.add_widget(self.Vacuum_Button)
+        self.Grid.add_widget(self.Gas_Button)
+        self.Grid.add_widget(self.RF_Button)
+        self.Grid.add_widget(self.Move_Button)
+
+        self.Container.add_widget(self.Grid)
+        return self.Container
+
+class Parameter_Container(object):
     '''Class contains a box, containing agrid with the
     MFC labels and set point buttons'''
     def __init__(self):
         self.Container = BoxLayout(pos_hint = {'top':1},
-                                   size_hint_x = 0.33, size_hint_y =  0.33)
+                                   size_hint_x = 0.33, size_hint_y =  1)
 
-        self.Grid = GridLayout(rows = 3, cols = 4, spacing = 5)
+        self.Grid = GridLayout(rows = 9, cols = 4, spacing = 5)
 
-        self.MFC1_Label_Object = MFC_Read_Label()
-        self.MFC2_Label_Object = MFC_Read_Label()
-        self.MFC3_Label_Object = MFC_Read_Label()
+        #_______________________MFC_Labels/Buttons____________________________
+        self.MFC_Gas_Title_Object = Title()
+        self.MFC_Gas_Title = self.MFC_Gas_Title_Object.build()
+        self.MFC_Set_Title_Object = Title()
+        self.MFC_Set_Title = self.MFC_Set_Title_Object.build()
+        self.MFC_Read_Title_Object = Title()
+        self.MFC_Read_Title = self.MFC_Read_Title_Object.build()
+        self.MFC_Space = Label()
+
+        self.MFC1_Label_Object = Title()
+        self.MFC2_Label_Object = Title()
+        self.MFC3_Label_Object = Title()
         self.MFC1_Label = self.MFC1_Label_Object.build()
         self.MFC2_Label = self.MFC2_Label_Object.build()
         self.MFC3_Label = self.MFC3_Label_Object.build()
+
+        self.MFC1_Label_Object.Set_Text('Silane')
+        self.MFC2_Label_Object.Set_Text('Helium')
+        self.MFC3_Label_Object.Set_Text('Hydrogen')
 
         self.MFC1_Read_Label_Object = MFC_Read_Label()
         self.MFC2_Read_Label_Object = MFC_Read_Label()
@@ -99,6 +150,57 @@ class MFC_Container(object):
         self.MFC2_Button = self.MFC2_Button_Object.build()
         self.MFC3_Button = self.MFC3_Button_Object.build()
 
+        #_______________________RF_Labels/Buttons____________________________
+
+        #Title (Label)
+        self.RF_Title_Object = Title()
+        self.RF_Title = self.RF_Title_Object.build()
+        self.RF_Set_Title_Object = Title()
+        self.RF_Set_Title = self.RF_Set_Title_Object.build()
+        self.RF_Read_Title_Object = Title()
+        self.RF_Read_Title = self.RF_Read_Title_Object.build()
+        self.RF_Reflected_Title_Object = Title()
+        self.RF_Reflected_Title = self.RF_Reflected_Title_Object.build()
+        #Title (Label)
+
+        #Values(Label)
+        self.RF_Space = Label() #This is just a place holder, will have no functionality
+        self.RF_Read_Label_Object = MFC_Read_Label()
+        self.RF_Read_Label = self.RF_Read_Label_Object.build()
+        self.RF_Reflected_Label_Object = MFC_Read_Label()
+        self.RF_Reflected_Label = self.RF_Reflected_Label_Object.build()
+        self.RF_Set_Button_Object = Green_Set_Button(self.RF_Read_Label)
+        self.RF_Set_Button = self.RF_Set_Button_Object.build()
+
+        #_______________________Pressure_Labels/Buttons____________________________
+
+        #Title (Label)
+        self.Pressure_Title_Object = Title()
+        self.Pressure_Title = self.Pressure_Title_Object.build()
+
+        self.Pressure_Set_Title_Object = Title()
+        self.Pressure_Set_Title = self.Pressure_Set_Title_Object.build()
+
+        self.Pressure_Read_Title_Object = Title()
+        self.Pressure_Read_Title = self.Pressure_Read_Title_Object.build()
+
+        self.Pressure_Quartz_Title_Object = Title()
+        self.Pressure_Quartz_Title = self.Pressure_Quartz_Title_Object.build()
+
+        #Values(Label)
+        self.Pressure_Space = Label() #This is just a place holder, will have no functionality
+
+        self.Pressure_Set_Object = MFC_Read_Label()
+        self.Pressure_Set = self.Pressure_Set_Object.build()
+
+        self.Pressure_Read_Object = MFC_Read_Label()
+        self.Pressure_Read = self.Pressure_Read_Object.build()
+
+        self.Pressure_Quartz_Object = MFC_Read_Label()
+        self.Pressure_Quartz = self.Pressure_Quartz_Object.build()
+
+
+
     def set_label_text(self,label,text):
         '''A method to access the text for the MFC labels'''
         if label == 'MFC1':
@@ -111,6 +213,12 @@ class MFC_Container(object):
     def build(self):
         '''Adds the different widgets (Buttons and Labels)
         to the grid and add the grid to the box'''
+
+        #MFC Additions
+        self.Grid.add_widget(self.MFC_Gas_Title)
+        self.Grid.add_widget(self.MFC_Set_Title)
+        self.Grid.add_widget(self.MFC_Read_Title)
+        self.Grid.add_widget(self.MFC_Space)
 
         self.Grid.add_widget(self.MFC1_Label)
         self.Grid.add_widget(self.MFC1_Read_Label)
@@ -127,11 +235,31 @@ class MFC_Container(object):
         self.Grid.add_widget(self.MFC3_Set_Label)
         self.Grid.add_widget(self.MFC3_Button)
 
+        #RF Additions
+        self.Grid.add_widget(self.RF_Title)
+        self.Grid.add_widget(self.RF_Set_Title)
+        self.Grid.add_widget(self.RF_Read_Title)
+        self.Grid.add_widget(self.RF_Reflected_Title)
+        self.Grid.add_widget(self.RF_Space)
+        self.Grid.add_widget(self.RF_Read_Label)
+        self.Grid.add_widget(self.RF_Reflected_Label)
+        self.Grid.add_widget(self.RF_Set_Button)
+
+        #Pressure Additions
+        self.Grid.add_widget(self.Pressure_Title)
+        self.Grid.add_widget(self.Pressure_Set_Title)
+        self.Grid.add_widget(self.Pressure_Read_Title)
+        self.Grid.add_widget(self.Pressure_Quartz_Title)
+        self.Grid.add_widget(self.Pressure_Space)
+        self.Grid.add_widget(self.Pressure_Set)
+        self.Grid.add_widget(self.Pressure_Read)
+        self.Grid.add_widget(self.Pressure_Quartz)
+
         self.Container.add_widget(self.Grid)
         return self.Container
 
 
-class Generator_Container(object):
+class Generator_Container(object): #Thinking about getting rid of this
     '''a class containing a box for all of the generator
     set & reads'''
 
@@ -176,20 +304,25 @@ class Interface(App):
                          keep_ratio=False,allow_stretch=True)
     Background_Box = BoxLayout()
 
-    MFC_Grid_Object = MFC_Container()
-    MFC_Grid = MFC_Grid_Object.build()
+    Parameter_Object= Parameter_Container()
+    Parameter_Grid = Parameter_Object.build()
 
-    Generator_Container_Object = Generator_Container()
-    Generator_Container = Generator_Container_Object.build()
+    Process_Container_Object = Process_Container()
+    Process_Container = Process_Container_Object.build()
 
+
+    close_toggle = ToggleButton()
 
     def build(self):
         self.F.add_widget(self.Background_Picture)
         self.F.add_widget(self.Background_Box)
-        self.F.add_widget(self.MFC_Grid)
-        self.F.add_widget(self.Generator_Container)
+        self.F.add_widget(self.Parameter_Grid)
+        self.F.add_widget(self.Process_Container)
 
         return self.F
+
+    def on_stop(self):
+        self.close_toggle.state = 'down'
 
 
 if __name__ =='__main__':
--- main.py
+++ main.py
@@ -1,12 +1,56 @@
 from Interface import Interface
+from Valve import Valve
+from Pressure import Pressure
+from threading import Thread
+
+def Create_Thread(function,*args):
+	New_Thread = Thread(target = function, args = (args))
+	New_Thread.start()
 
 
-def bubba():
-    print('The button is being pressed\n\n\n\n\n\n')
+#Opening Connections to Equipment
+Valve = Valve('A')
+Pressure = Pressure('B')
 
-Interface = Interface()
+
+#Getting relevant labelxs/buttons out for Logic Use
+class Int(Interface):
+    def prnt(self):
+        print 'sup'
+
+
+Interface = Int()
+Process_Container = Interface.Process_Container_Object
+Vacuum_Button = Process_Container.Vacuum_Button_Object.Button
+Gas_Button = Process_Container.Gas_Button_Object.Button
+
+Close = Interface.close_toggle
+
+
+#Binding the Process Buttons
+
+
+#Reading the Pressure
+def Read_Pressure():
+    print Pressure.Get_Chamber_Pressure()
+    print Pressure.Get_Plasma_Pressure()
+
+#Opening and Closing the Vacuum Valve
+def Valve_Open(instance):
+    '''This function is bound to the Vacuum Toggle Button,
+    If the state of the button is down, the valve opens, if
+    it is up, the valve will close'''
+    if instance.state == 'down':
+        Valve.Open()
+    else:
+        Valve.Close()
+Vacuum_Button.bind(on_press = Valve_Open) #Opens the Vacuum when pressed
+
+
+
 
 
 
 
 Interface.run()
+
